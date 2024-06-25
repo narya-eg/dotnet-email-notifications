5e0150b65bb0dec8f2398be6b77e2cf2a516fe79
Hussein Dossoki <husseindossoki@gmail.com>
2024-06-26 01:56:40 +0400

    Add Narya.Email.Core.UnitTests project and update the file structure

---
+++ tests/Narya.Email.Core.UnitTests/EmailOptionsTests.cs
@@ -0,0 +1,20 @@
+using Narya.Email.Core.Builders;
+using Xunit;
+
+namespace Narya.Email.Core.UnitTests;
+
+public class EmailOptionsTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var builder = new EmailOptionsBuilder().Build();
+
+        // Act
+        // var result = myClass.MyMethod();
+
+        // Assert
+        Assert.True(true);
+    }
+}
